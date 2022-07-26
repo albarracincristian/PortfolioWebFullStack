96bd7490e92282c47125414de097a9a46cef4ce1
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-07-26 00:15:22 -0300

    creando el login

---
+++ Front-End-Angular/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+
+const Routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(Routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- Front-End-Angular/src/app/app.module.ts
+++ Front-End-Angular/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -26,10 +29,13 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
