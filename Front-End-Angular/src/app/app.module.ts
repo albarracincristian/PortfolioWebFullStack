4abd7b5cb35fc54ab76f92e6f2215de5b28cdbee
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-07-29 00:29:18 -0300

    configurando login en frontend

--- Front-End-Angular/src/app/app.module.ts
+++ Front-End-Angular/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
