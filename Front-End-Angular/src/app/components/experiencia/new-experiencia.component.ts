67a5caf90c65f6f8b32cb7c4c9427892abcc0527
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-07-30 22:48:09 -0300

    corrigiendo experiencia

--- Front-End-Angular/src/app/components/experiencia/new-experiencia.component.ts
+++ Front-End-Angular/src/app/components/experiencia/new-experiencia.component.ts
@@ -6,28 +6,28 @@ import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 @Component({
   selector: 'app-new-experiencia',
   templateUrl: './new-experiencia.component.html',
-  styleUrls: ['./new-experiencia.component.css'],
+  styleUrls: ['./new-experiencia.component.css']
 })
 export class NewExperienciaComponent implements OnInit {
   nombreE: string = '';
   descripcionE: string = '';
 
-  constructor(
-    private sExperiencia: SExperienciaService,
-    private router: Router
-  ) {}
+  constructor(private sExperiencia: SExperienciaService, private router: Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   onCreate(): void {
     const expe = new Experiencia(this.nombreE, this.descripcionE);
-    this.sExperiencia.save(expe).subscribe((data) => {
-      alert('Experiencia agregada');
-      this.router.navigate(['']);
-    }, err =>{
-      alert("Falló");
-      this.router.navigate(['']);
-    }
-    );
+    this.sExperiencia.save(expe).subscribe(
+      data => {
+        alert("Experiencia añadida");
+        this.router.navigate(['']);
+      }, err => {
+        alert("Falló");
+        this.router.navigate(['']);
+      }
+    )
   }
+
 }
