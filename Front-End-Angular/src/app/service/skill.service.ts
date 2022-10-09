7b76e3eddfc860ec10870d6e86d723f519b7ea4d
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-10-08 22:38:56 -0300

    correcciones de errores

--- Front-End-Angular/src/app/service/skill.service.ts
+++ Front-End-Angular/src/app/service/skill.service.ts
@@ -9,27 +9,27 @@ import { Skill } from '../model/skill';
   providedIn: 'root'
 })
 export class SkillService {
-  URL = 'https://porfoliocristian.herokuapp.com/skill/';
+  skillURL = 'https://porfoliocristian.herokuapp.com/skill/';
 
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Skill[]>{
-    return this.httpClient.get<Skill[]>(this.URL + 'lista');
+    return this.httpClient.get<Skill[]>(this.skillURL + 'lista');
   }
 
   public detail(id: number): Observable<Skill>{
-    return this.httpClient.get<Skill>(this.URL + `detail/${id}`);
+    return this.httpClient.get<Skill>(this.skillURL + `detail/${id}`);
   }
 
   public save(skill: Skill): Observable<any>{
-    return this.httpClient.post<any>(this.URL + 'create', skill);
+    return this.httpClient.post<any>(this.skillURL + 'create', skill);
   }
 
   public update(id: number, skill: Skill): Observable<any>{
-    return this.httpClient.put<any>(this.URL + `update/${id}`, skill);
+    return this.httpClient.put<any>(this.skillURL + `update/${id}`, skill);
   }
 
   public delete(id: number): Observable<any>{
-    return this.httpClient.delete(this.URL + `delete/${id}`);
+    return this.httpClient.delete(this.skillURL + `delete/${id}`);
   }
 }
