45eb742de11d1efda26fd6c2869f7a788c274807
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-10-09 00:12:48 -0300

    correccion

--- Back-End-SpringBoot/src/main/java/com/portfolioalbarracin/cristian/Controller/CHys.java
+++ Back-End-SpringBoot/src/main/java/com/portfolioalbarracin/cristian/Controller/CHys.java
@@ -23,9 +23,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping("/skill")
-@CrossOrigin(origins = "https://porfoliocristian-2a604.web.app")
+@CrossOrigin(origins = "https://porfoliocristian-2a604.web.app/")
 public class CHys {
     @Autowired
     Shys shys;
