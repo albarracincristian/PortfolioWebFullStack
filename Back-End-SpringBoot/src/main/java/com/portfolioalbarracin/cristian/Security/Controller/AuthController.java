c12a5d9594ffd8634d9930ac7e55f04fe367ba0e
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-10-08 21:34:43 -0300

    complemento HYS

--- Back-End-SpringBoot/src/main/java/com/portfolioalbarracin/cristian/Security/Controller/AuthController.java
+++ Back-End-SpringBoot/src/main/java/com/portfolioalbarracin/cristian/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://porfoliocristian-2a604.web.app/")
 public class AuthController {
 
     @Autowired
--- Front-End-Angular/src/app/app.module.ts
+++ Front-End-Angular/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditskillComponent } from './components/hys/editskill.component';
+import { NewskillComponent } from './components/hys/newskill.component';
 
 
 @NgModule({
@@ -41,7 +43,9 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
     NewExperienciaComponent,
     EditExperienciaComponent,
     NeweducacionComponent,
-    EditeducacionComponent
+    EditeducacionComponent,
+    EditskillComponent,
+    NewskillComponent
   ],
   imports: [
     BrowserModule,
