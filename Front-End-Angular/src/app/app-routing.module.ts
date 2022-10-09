31d77c2505cc27f2d910e6907525057f7b0ba64b
Cristian Albarracin <albarracin.cristian@hotmail.com>
2022-10-08 23:58:08 -0300

    correccion

--- Front-End-Angular/src/app/app-routing.module.ts
+++ Front-End-Angular/src/app/app-routing.module.ts
@@ -10,8 +10,8 @@ import { NewskillComponent } from './components/hys/newskill.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
   { path: 'nuevaexp', component: NewExperienciaComponent},
   { path: 'editexp/:id', component: EditExperienciaComponent},
   { path: 'nuevaedu', component: NeweducacionComponent},
